4bb8a10a5710c92de375035abb9f449bd47cc177
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-03-26 09:32:55 -0700

    prompt questions appearing correctly

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO role (id, role_title, role_salary, department_id)
+VALUES(),
+    (),
+    (),
+    ();
+      
+INSERT INTO department (id, department_name)
+VALUES(),
+    (),
+    (),
+    ();
+
+INSERT INTO employee (id, employee_first_name, employee_last_name, employee_role_id, employee_manager_id)
+VALUES(),
+    (),
+    (),
+    ();
